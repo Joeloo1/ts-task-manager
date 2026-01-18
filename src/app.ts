c16581dc1f001ec37cc8fd8c740a510b4b2a3c0b
joeloo1 <agboolajoel123@gmail.com>
2026-01-18 21:50:31 +0100

    feat: add projects route to app.ts

--- src/app.ts
+++ src/app.ts
@@ -2,7 +2,10 @@ import express from "express";
 import morgan from "morgan";
 import authRoutes from "./Router/auth.routes";
 import taskRoutes from "./Router/task.routes";
+import projectRoutes from "./Router/project.routes";
+
 import { globalErrorHandler } from "./Error/globalErrorHandler";
+
 const app = express();
 
 if (process.env.NODE_ENV === "development") {
@@ -14,6 +17,7 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use("/api/v1/user", authRoutes);
 app.use("/api/v1/task", taskRoutes);
+app.use("/api/v1/project", projectRoutes);
 
 app.use(globalErrorHandler);
 export default app;
