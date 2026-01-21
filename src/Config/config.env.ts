972deac3717ca801b09fe02e140c4108741b310c
joeloo1 <agboolajoel123@gmail.com>
2026-01-21 22:09:02 +0100

    chore: add dotenv configuration

---
+++ src/Config/config.env.ts
@@ -0,0 +1,11 @@
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const config = {
+  PORT: process.env.PORT || 3000,
+  NODE_ENV: process.env.NODE_ENV,
+  JWT_SECRET: process.env.JWT_SECRET as string,
+  JWT_EXPIRES_IN: process.env.JWT_EXPIRES_IN as string,
+};
+export default config;
